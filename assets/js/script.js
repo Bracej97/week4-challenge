5b70f6b341c7fbd2a009f0702731e4a2109fd933
Joe Bracey <joebracey1@gmail.com>
2024-10-14 21:03:22 +0100

    Adding complete button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 
-
+var taskTableEl = document.getElementById("task-table");
 // 1. use document.getElementById to select the searchTerm Button
 var inputTaskEl = document.getElementById("input-task");
 // 2. use document.getElementById to select the searchButton Button
 var addTaskButtonEl = document.getElementById("add-task-button");
 // 3. add an event listener to the searchButton that calls the search function when clicked
-addTaskButtonEl.addEventListener("click", add)
+addTaskButtonEl.addEventListener("click", add);
 
 function add() {
   // 4. use the value property of the searchInput to get the search term
     let task = inputTaskEl.value;
     if (task != "") {
   // 5. select the searches div using document.getElementById
-    var taskTableEl = document.getElementById("task-table");
+
   // 6. create a new li element using document.createElement
     var newRowEl = document.createElement("tr");
     newRowEl.innerHTML = `
@@ -27,3 +27,12 @@ function add() {
     taskTableEl.appendChild(newRowEl);
     }
 };
+
+var completeButtonEl = document.getElementById("complete-btn");
+
+completeButtonEl.addEventListener("click", complete);
+
+function complete() {
+  let thisRow = completeButtonEl.parentNode;
+  console.log(thisRow.children)
+}
